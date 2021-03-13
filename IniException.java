9cc10182683c2e1768eb7bcdf6b7046e72746908
cwclancy <connorwilliamclancy@gmail.com>
2021-03-13 08:33:27 -0500

    add docs for IniException

--- IniException.java
+++ IniException.java
@@ -1,8 +1,18 @@
 package HW4;
 
+/**
+ * An {@code IniException} represents an error within an {@code IniObject}.
+ */
 public class IniException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * The constructor for the exception thrown when there is an error in an {@code IniObject}.
+     * This exception is simply thrown:
+     * <blockquote><pre>
+     * throw new IniException();
+     * </pre></blockquote>
+     */
     public IniException() {
         super();
     }
